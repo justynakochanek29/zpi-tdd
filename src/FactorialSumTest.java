dd40d52ef72ff0002500f198d6b80b844f30542e
Justyna Kochanek <justyna.kochanek29@gmail.com>
2015-06-12 18:59:14 +0200

    Test for method convertIntoArray added

--- src/FactorialSumTest.java
+++ src/FactorialSumTest.java
@@ -13,5 +13,11 @@ public class FactorialSumTest {
 	public void calculateFactorial() {
 		assertEquals("FactorialSum.calculateFactorial()", 3628800, FactorialSum.calculateFactorial(10));
 	}
+	
+	@Test
+	public void convertIntoArray(){
+		int[] test = {1,2,3};
+		assertArrayEquals("FactorialSum.convertIntoArray()", test, FactorialSum.convertIntoArray(123));
+	}
 
 }
