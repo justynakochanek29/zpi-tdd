5ecbdb94b00c3c6e21585b004345c078a6abe758
Justyna Kochanek <justyna.kochanek29@gmail.com>
2015-06-12 19:04:12 +0200

    method convertIntoArray implemented in class FactorialSum

--- src/FactorialSum.java
+++ src/FactorialSum.java
@@ -7,4 +7,15 @@ class FactorialSum {
 		}
 		return result;
 	}
+	
+	public static int[] convertIntoArray(int number){
+		String arr = Integer.toString(number);
+		int[] newNumber = new int[arr.length()];
+		for (int i = 0; i < arr.length(); i++)
+		{
+			newNumber[i] = arr.charAt(i) - '0';
+		}
+		
+		return newNumber;
+	}
 }
