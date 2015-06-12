9c3acacbb7067ab50ea9b13536e17df468e607ef
Justyna Kochanek <justyna.kochanek29@gmail.com>
2015-06-12 19:56:24 +0200

    Code refactoring

--- src/FactorialSum.java
+++ src/FactorialSum.java
@@ -1,35 +1,67 @@
-class FactorialSum {
+public class FactorialSum {
 	
+	/**
+	 *  Private attribute - sum of factorial digits
+	 */
 	private int sum = 0;
 	
+
+	/**
+	 * Constructor for class FactorialSum
+	 * 
+	 * @param n - integer number
+	 */
 	public FactorialSum(int n) {
 		int[] array = convertIntoArray(calculateFactorial(n));
 		this.sum = sumOfFactorialDigits(array);
 	}
 		
+	/**
+	 * Getter for private int sum
+	 * 
+	 * @return sum of factorial digits
+	 */
 	public int getSum() {
 		return this.sum;
 	}
 
+	/**
+	 * Method calculate factorial for number from parameter
+	 * 
+	 * @param n - integer number
+	 * @return factorial of number from parameter
+	 */
 	public static int calculateFactorial(int n){
 		int result = 1;
 		for (int i = 1; i <= n; i++){
-			result = result * i;
+			result *= i;
 		}
 		return result;
 	}
 	
+	/**
+	 * Method convert integer number into array of its digits
+	 * 
+	 * @param number
+	 * @return array of integer digits
+	 */
 	public static int[] convertIntoArray(int number){
-		String arr = Integer.toString(number);
-		int[] newNumber = new int[arr.length()];
-		for (int i = 0; i < arr.length(); i++)
+		String temp = Integer.toString(number);
+		int[] numberArray = new int[temp.length()];
+		for (int i = 0; i < temp.length(); i++)
 		{
-			newNumber[i] = arr.charAt(i) - '0';
+			numberArray[i] = temp.charAt(i) - '0';
 		}
 		
-		return newNumber;
+		return numberArray;
 	}
 	
+	/**
+	 * Method calculate sum for elements of integer array
+	 * 
+	 * @param array - array with integer elements
+	 * @return sum of array elements
+	 */
 	public static int sumOfFactorialDigits(int[] array){
 		int result = 0;
 		for (int i = 0; i < array.length; i++){
--- src/FactorialSumTest.java
+++ src/FactorialSumTest.java
@@ -4,28 +4,42 @@ import org.junit.Test;
 
 
 /**
+ * This is a test class
+ * 
  * @author Justyna
  *
  */
 public class FactorialSumTest {
 
+	/**
+	 * Test for method calculateFactorial()
+	 */
 	@Test
 	public void calculateFactorial() {
 		assertEquals("FactorialSum.calculateFactorial()", 3628800, FactorialSum.calculateFactorial(10));
 	}
 	
+	/**
+	 * Test for method convertIntoArray()
+	 */
 	@Test
 	public void convertIntoArray(){
 		int[] test = {1,2,3};
 		assertArrayEquals("FactorialSum.convertIntoArray()", test, FactorialSum.convertIntoArray(123));
 	}
 	
+	/**
+	 * Test for method sumOfFactorialDigits()
+	 */
 	@Test
 	public void sumOfFactorialDigits(){
 		int[] test = {1,2,3};
 		assertEquals("FactorialSum.sumOfFactorialDigits()",6 ,FactorialSum.sumOfFactorialDigits(test));
 	}
 	
+	/**
+	 * Test for getter getSum()
+	 */
 	@Test
 	public void getSum(){
 		FactorialSum test = new FactorialSum(10);
